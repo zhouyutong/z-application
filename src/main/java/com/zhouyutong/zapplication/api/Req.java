0ad1cac0f8b1ab15f58b42869a7351a1689a9e1c
zhoutao <zhoutao@edaijia-inc.cn>
2018-03-07 11:13:24 +0800

    系統級別參數

--- src/main/java/com/zhouyutong/zapplication/api/Req.java
+++ src/main/java/com/zhouyutong/zapplication/api/Req.java
@@ -1,53 +1,26 @@
 package com.zhouyutong.zapplication.api;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.util.Map;
 
 /**
  * 远程调用的请求参数
- * 定义了系统级参数和应用级参数(data)
+ * 定义了
+ * 系统级参数如appKey等
+ * 应用级参数
  * <p/>
- * Created by lianjia on 2016/6/2.
+ * Created by zhoutao on 2016/6/2.
  */
-@Getter
-@Setter
-@ToString
+@Data
 public class Req implements Serializable {
     /**
-     * 系统级参数|必填|分配给调用方的appKey，用于区分调用方
+     * 系统级参数
      */
-    private String appKey;
+    private Map<String, Object> sysParam;
     /**
-     * 系统级参数|非必填|分配给用户的令牌，通过授权获取，方法详见(授权说明TODO)
+     * 应用级参数
      */
-    private String accessToken;
-    /**
-     * 系统级参数|非必填|签名，具体签名算法接口自行约定
-     */
-    private String sign;
-    /**
-     * 系统级参数|必填|时间戳，格式为yyyy-MM-dd HH:mm:ss.SSS，例如：2011-06-16 13:23:30.667
-     */
-    private String timestamp;
-    /**
-     * 系统级参数|必填|API协议版本，可选值:v1
-     */
-    private String version;
-    /**
-     * 系统级参数|必填|当前操作城市
-     */
-    private String cityCode;
-    /**
-     * 系统级参数|必填|当前操作人系统号
-     */
-    private String userCode;
-    /**
-     * 应用级参数|非必填|
-     */
-
-    private Map<String, Object> data;
+    private Map<String, Object> appParam;
 }
