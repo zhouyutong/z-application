eae31f633aadfe1c25db5ae027f17dcd7343edac
zhoutao <zhoutao@edaijia-inc.cn>
2020-03-24 19:33:54 +0800

    http请求日志优化14

--- src/main/java/com/zhouyutong/zapplication/api/ErrorCode.java
+++ src/main/java/com/zhouyutong/zapplication/api/ErrorCode.java
@@ -15,8 +15,8 @@ import java.io.Serializable;
  */
 public class ErrorCode implements Serializable {
 
-    public static final ErrorCode PARAM_REQUIRED = new ErrorCode("4001", "参数[%s]不能为空");
-    public static final ErrorCode PARAM_INVALID = new ErrorCode("4002", "参数[%s]格式无效");
+    public static final ErrorCode PARAM_REQUIRED = new ErrorCode("4001", "参数：%s不能为空");
+    public static final ErrorCode PARAM_INVALID = new ErrorCode("4002", "参数：%s格式无效");
     public static final ErrorCode SIGN = new ErrorCode("4003", "参数签名验证失败");
     public static final ErrorCode AUTHORITY = new ErrorCode("4004", "您没有权限访问该接口");
     public static final ErrorCode DECRYPT = new ErrorCode("4005", "请求数据解密失败");
@@ -25,17 +25,17 @@ public class ErrorCode implements Serializable {
     public static final ErrorCode UPLOAD_SIZE = new ErrorCode("4008", "您上传的文件大小不能超过[%s]KB");
     public static final ErrorCode SYSTEM_BUSY = new ErrorCode("4009", "服务器繁忙，请稍后重试");
     public static final ErrorCode NEED_LOGIN = new ErrorCode("4010", "登录超时,请重新登录");
-    public static final ErrorCode OPER_FAIL = new ErrorCode("4011", "操作失败,[%s]");
+    public static final ErrorCode OPER_FAIL = new ErrorCode("4011", "操作失败：%s");
     /**
      * 5000通用服务器异常,5001访问redis异常
      */
-    public static final ErrorCode SERVER = new ErrorCode("5000", "服务器内部错误,[%s].");
-    public static final ErrorCode SERVER_REDIS = new ErrorCode("5001", "服务器内部错误,[%s].");
-    public static final ErrorCode SERVER_MYSQL = new ErrorCode("5002", "服务器内部错误,[%s].");
-    public static final ErrorCode SERVER_ES = new ErrorCode("5003", "服务器内部错误,[%s].");
-    public static final ErrorCode SERVER_HTTP = new ErrorCode("5004", "服务器内部错误,[%s].");
-    public static final ErrorCode SERVER_CASSANDRA = new ErrorCode("5005", "服务器内部错误,[%s].");
-    public static final ErrorCode SERVER_ORACLE = new ErrorCode("5006", "服务器内部错误,[%s].");
+    public static final ErrorCode SERVER = new ErrorCode("5000", "服务器内部错误：%s");
+    public static final ErrorCode SERVER_REDIS = new ErrorCode("5001", "服务器内部错误：%s");
+    public static final ErrorCode SERVER_MYSQL = new ErrorCode("5002", "服务器内部错误：%s");
+    public static final ErrorCode SERVER_ES = new ErrorCode("5003", "服务器内部错误：%s");
+    public static final ErrorCode SERVER_HTTP = new ErrorCode("5004", "服务器内部错误：%s");
+    public static final ErrorCode SERVER_CASSANDRA = new ErrorCode("5005", "服务器内部错误：%s");
+    public static final ErrorCode SERVER_ORACLE = new ErrorCode("5006", "服务器内部错误：%s");
 
     @Getter
     private String code;
