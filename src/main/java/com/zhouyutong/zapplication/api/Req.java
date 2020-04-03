186d30915c86ae961e7ccc7ee6ee832bd2b1cc01
zhoutao <zhoutao@edaijia-inc.cn>
2020-04-03 23:24:41 +0800

    接口请求标识

--- src/main/java/com/zhouyutong/zapplication/api/Req.java
+++ src/main/java/com/zhouyutong/zapplication/api/Req.java
@@ -1,15 +1,9 @@
 package com.zhouyutong.zapplication.api;
 
-import lombok.Data;
-
 import java.io.Serializable;
 
 /**
- * 系统级参数
- * TODO:具体哪些系统级参数待定
+ * 接口请求标识
  */
-@Data
-public class Req implements Serializable {
-    private String version;
-    private String timestamp;
+public interface Req extends Serializable {
 }
